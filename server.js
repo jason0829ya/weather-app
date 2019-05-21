5e60245994987f869896d7ecff77fb65156e3f06
jason0829ya <r06943064@ntu.edu.tw>
2019-05-21 10:14:28 +0800

    fix title

--- server.js
+++ server.js
@@ -6,9 +6,9 @@ const getGeoCode = require('./src/API/geocode');
 const getForecastdata = require('./src/API/forecast');
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/ping', function (req, res) {
- return res.send('pong');
-});
+// app.get('/ping', function (req, res) {
+//  return res.send('pong');
+// });
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
